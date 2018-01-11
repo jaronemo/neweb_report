e78306cb3f63239fe47fe82eaad28b6f66de85ed
Jason <jaronemo@msn.com>
2018-01-11 11:37:06 +0800

    fix manifest description
    fix manifest description

--- __manifest__.py
+++ __manifest__.py
@@ -2,15 +2,15 @@
 {
     'name': u"<6>.NEWEB 報表",
     'version': '1.0',
-    'summary': "repoet",
+    'summary': "Neweb Report Template For cfprint use",
 
-    'description': """ """,
-    'author': "LANSIR Technology",
-    'category': 'Uncategorized',
-	'version': '0.1',
+    'description': """Neweb Report Template for cfpirnt windows agent use.... """,
+    'author': "LANSIR Technology,Jason Wu, Monica",
+    'category': 'Tools',
+    'version': '1.0',
     'data': [
-            'report.xml',
-		'report/newweb_assign_custom.xml',
-		],
+        'report.xml',
+        'report/newweb_assign_custom.xml',
+    ],
 
 }
